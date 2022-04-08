f033c1c56061c58dede2fa89b9a47f53482e4a76
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-08 13:30:29 -0500

    successful route to duffel api

---
+++ code/duffel.mjs
@@ -0,0 +1,9 @@
+import { Duffel } from '@duffel/api'
+export const duffel = new Duffel({
+    token: process.env.DUFFEL_TOKEN
+})
+
+// module.exports = duffel;
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+import 'dotenv/config';
+import express from 'express'
+import { duffel } from './code/duffel.mjs';
 const app = express();
-require('dotenv').config()
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+
+const aircraft = async () => {
+    try{
+        const request = await duffel.aircraft.get('arc_00009VMF8AhXSSRnQDI6Hi')
+        console.log('request', request);
+    } catch(err) { 
+        console.log('err', err)
+    }
+}
+
+aircraft();
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
-})
+});
 
